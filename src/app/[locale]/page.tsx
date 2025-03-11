683de9f9b3dab55f3bf4dd77e19957482ce31f38
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2025-03-11 11:13:21 -0600

    i18 added

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,8 @@
+import Navbar from '../../components/custom/navbar';
+export default function Home() {
+  return (
+    <main className="w-full bg-[#343131] h-screen pt-1">
+      <Navbar></Navbar>
+    </main>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '../components/custom/navbar';
-import { TranslateButton } from '../components/custom/translate-button';
-export default function Home() {
-  return (
-    <main className="w-full bg-[#343131] h-screen pt-1">
-     <Navbar></Navbar>
-    </main>
-  );
-}
